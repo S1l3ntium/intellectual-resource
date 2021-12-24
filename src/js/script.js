d6fbdd1eb371ad3310288493e07c3cc083a5aca8
Denis Chichmarev <denis.chichmarev@gmail.com>
2021-12-24 17:31:54 +0300

    start

---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+//libs
+@@include("../../node_modules/jquery/dist/jquery.slim.min.js");
+@@include("../../node_modules/bootstrap/dist/js/bootstrap.min.js");
+@@include("../../node_modules/popper.js/dist/popper.min.js");
